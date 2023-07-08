1e0095273bf6cc2be402c26189d4442a18e5d722
ABDELLAH AJJOURI <abdellah.elajjouri@gmail.com>
2023-07-08 09:08:52 +0100

    'init'

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Test');
 });
+
+Route::get('/taches', [TacheController::class, 'index'])->name('taches.index');
+Route::get('/taches/create', [TacheController::class, 'create'])->name('taches.create');
+Route::post('/taches', [TacheController::class, 'store'])->name('taches.store');
+Route::get('/taches/{id}/edit', [TacheController::class, 'edit'])->name('taches.edit');
+Route::put('/taches/{id}', [TacheController::class, 'update'])->name('taches.update');
+Route::delete('/taches/{id}', [TacheController::class, 'destroy'])->name('taches.destroy');

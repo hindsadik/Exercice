6099b8cdda38fc641002d4f4bf8526a667b40c48
Salahoddine LOUKHMI <se.loukhmi@gmail.com>
2023-07-08 09:27:04 +0100

    front version 1

---
+++ resources/js/Pages/Container.jsx
@@ -0,0 +1,12 @@
+'use client';
+import TableData from './TableData';
+
+export default function Container() {
+    return (
+        <div className="bg-gray-100 text-white p-4 h-screen w-full m-0 px-0">
+            <TableData />
+        </div>
+    )
+}
+
+
